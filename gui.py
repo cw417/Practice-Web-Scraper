da39893d84c5163d1444edaf43390d774e6766cd
Chris W <chriswhitlock417@gmail.com>
2020-01-08 18:44:54 -0500

    gui.py

--- gui.py
+++ gui.py
@@ -1,19 +1,19 @@
-from tkinter import *
+import tkinter as tk
 import json
 
-class PyScrapeGUI(Frame):
+class ScrapeGUI(tk.Frame):
 
     def __init__(self, master=None):
-        Frame.__init__(self, master)                 
+        tk.Frame.__init__(self, master)                 
         self.master = master
         self.init_window()
 
     def init_window(self):
   
-        self.master.title("PyScrape GUI")
+        self.master.title("Web Scrape GUI")
 
         
-        self.pack(fill=BOTH, expand=1)
+        self.pack(fill=tk.BOTH, expand=1)
 
         label_width = 20
         entry_width = 60
@@ -21,7 +21,7 @@ class PyScrapeGUI(Frame):
 
         def get_entries():
             """This function gets entries from the fields and returns them
-            as a dictionary in a JSON file when the submit button is pressed"""
+            as a dictionary when the submit button is pressed"""
             dict = {}
 
             url = e_url.get()
@@ -35,24 +35,24 @@ class PyScrapeGUI(Frame):
             rec = e_rec.get()
             dict.update({'email_receive': rec})
 
-            dict_file = json.dump(dict, open("pyscrape_dict.json", 'w'), indent=4, sort_keys=True)
+            dict_file = json.dump(dict, open("pyscrape_dict.json", 'w'))
 
         # Create Labels for entry fields
-        l_url = Label(self, width=label_width, text="Enter URL: ")
-        l_item = Label(self, width=label_width, text="Item Name: ")
-        l_add = Label(self, width=label_width, text="Sender Email: ")
-        l_pw = Label(self, width=label_width, text="Sender PW: ")
-        l_rec = Label(self, width=label_width, text="Receiver Email: ")
+        l_url = tk.Label(self, width=label_width, text="Enter URL: ")
+        l_item = tk.Label(self, width=label_width, text="Item Name: ")
+        l_add = tk.Label(self, width=label_width, text="Sender Email: ")
+        l_pw = tk.Label(self, width=label_width, text="Sender PW: ")
+        l_rec = tk.Label(self, width=label_width, text="Receiver Email: ")
 
         # Create entry fields
-        e_url = Entry(self, width=entry_width)
-        e_item = Entry(self, width=entry_width)
-        e_add = Entry(self, width=entry_width)
-        e_pw = Entry(self, width=entry_width)
-        e_rec = Entry(self, width=entry_width)
+        e_url = tk.Entry(self, width=entry_width)
+        e_item = tk.Entry(self, width=entry_width)
+        e_add = tk.Entry(self, width=entry_width)
+        e_pw = tk.Entry(self, width=entry_width)
+        e_rec = tk.Entry(self, width=entry_width)
 
-        # Create submit button
-        b_sub = Button(self, width=button_width, text="Submit", command=get_entries)
+        # Create buttons for edit headers and submit
+        b_sub = tk.Button(self, width=button_width, text="Submit", command=get_entries)
 
         # Set up layout of label fields
         l_url.grid(row=0, column=0, sticky='w')
@@ -68,14 +68,14 @@ class PyScrapeGUI(Frame):
         e_pw.grid(row=3, column=1)
         e_rec.grid(row=4, column=1)
 
-        # Set up button layout
-        # Starts at 10 to enable later addition of more entry fields
+        # Set up layout of buttons
+        # Starts at 10 to enable later addition of extra entry fields
         b_sub.grid(row=10, column=0)
 
-root = Tk()
+root = tk.Tk()
 
 root.geometry("600x127")
 
-app = PyScrapeGUI(root)
+app = ScrapeGUI(root)
 root.mainloop()
 
